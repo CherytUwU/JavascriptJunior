1cdb39116922aa0db445848651fc0447f60c6ac0
CherytUwU <79613492+CherytUwU@users.noreply.github.com>
2021-07-04 12:18:31 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let name = prompt('Escrbie Tú Nombre/Nick Porfavor');
+
+document.getElementById('NameID1').innerHTML=`${name}`;
+document.getElementById('NameID2').innerHTML=`${name}`;
+
+// COMPARADORES 
+
+let numero1 = 22;
+let numero2 = 25;
+document.write(numero1 == numero2);
